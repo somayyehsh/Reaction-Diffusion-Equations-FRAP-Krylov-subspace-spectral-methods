04c21a694611dbffc376d16f53a36adc6d33bdc9
somayyehsh <53899687+somayyehsh@users.noreply.github.com>
2019-08-08 15:47:35 +0430

    Update methodwrapN.m

--- Code/methodwrapN.m
+++ Code/methodwrapN.m
@@ -12,24 +12,11 @@ y=dy*(0:N-1);
 [x2,y2]=meshgrid(x,y);
 x2=reshape(x2,numel(x2),1);
 y2=reshape(y2,numel(y2),1);
-%kon=255.0*ones(size(x2));    % 1/s   kon*re^2 ~ D1
-%%% SS: don't use vectors for constant coefficients, use scalars instead
-%%% also, define ones(N^2,1) once and re-use
+
 eN2=ones(N^2,1);
-kb=1.0;         % 1/s, doubt on its value
-%kb=reshape(kb,N,N);
-I0=1.0;   % micro A, doubt this to have micro or not
-%rn=0.5*ones(N^2,1);    % micro m
-%x=1.0*1.e-6*ones(size(x2));
-%y=1.0*1.e-6*ones(size(y2));
-%w1=[0:N/2 -N/2+1:-1];
-%w2=[0:N/2 -N/2+1:-1];
-%w1=[-N/2+1:N/2];
-%I=eye(N^2);
-%TIrn=fft2(reshape(Irn,N,N));
-% D1 = 45*ones(N^2,1);
-% D2 = 2.5*ones(N^2,1);
-ci=1.0;    %doubt on unit and value
+kb=1.0;        
+I0=1.0;   
+ci=1.0;    
 if coefs==1
     kon=10^(-0.5);    %e-0.5 (1./sqrt(10))1/s   kon*re^2 ~ D1 %correct reaction dominant
     koff=1.e-1;   % rn=0.5 \mu m
@@ -62,31 +49,14 @@ else
     rn=0.5;
 end
 Irn=(2.0*I0./pi.*rn.^2).*exp(-2.0*((x2-pi).^2+(y2-pi).^2)./rn.^2);
-%     kon=10.0*ones(N^2,1);    % 1/s   kon*re^2 ~ D1 %nc reaction dominant
-%     koff=1.e-2*ones(N^2,1);
-%     D1 = 30*1.e-6*ones(N^2,1); %removed 1.e-6 kang
-%     D2 = 1.e-6*ones(N^2,1); %removed 1.e-6  kang
 D=-2*eye(N)+diag(ones(N-1,1),1)+diag(ones(N-1,1),-1);
-%D(1,N)=1;
-%D(N,1)=1;
 D(1,1)=-1;
 D(N,N)=-1;
 D=D/dx^2;
 D=sparse(D);
 Lapl=kron(D,eye(N))+kron(eye(N),D);
-%D3=diag(ones(N-1,1),1);
-%D3(N,1)=1;
-%D3=D3-D3';
-%D3=D3/(2*dx);
-%D3=sparse(D3);
-%Dx=kron(D3,eye(N));
-%Dy=kron(eye(N),D3);
-%%% SS: because ci, koff, kon now scalars, need vector of 1's here
 B12=(ci.*koff)./(koff+kon)*eN2;
 B22=(ci.*kon)./(koff+kon)*eN2;
-%B0=[B12;B22];
-%%% SS: made matrices completely sparse, don't use eye or diag,
-%%% use speye or spdiags instead
 L12=koff*speye(N^2);
 L21=kon*speye(N^2);
 L11=-kb*spdiags(Irn,0,N^2,N^2)+D1*Lapl-L21;
