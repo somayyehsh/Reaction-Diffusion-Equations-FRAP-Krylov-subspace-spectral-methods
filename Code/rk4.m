73f246d39b29beeab5eac577787e454bbac20371
somayyehsh <53899687+somayyehsh@users.noreply.github.com>
2019-08-08 15:49:21 +0430

    Update rk4.m

--- Code/rk4.m
+++ Code/rk4.m
@@ -1,12 +1,10 @@
 function y=rk4(A,y0,tf,nstep)
 dt=tf/nstep;
 y=y0;
-%eigs(A,5)
 for i=1:nstep
     k1=A*y;
     k2=k1+dt/2*A*k1;
     k3=k1+dt/2*A*k2;
     k4=k1+dt*A*k3;
     y=y+dt/6*(k1+2*(k2+k3)+k4);
-    %norm(y,'inf')
 end
